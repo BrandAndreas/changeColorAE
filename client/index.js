d212fcfd7df33c57e988f1e8fb2c2b7f90a3f663
BrandAndreas <brand.unter3@gmail.com>
2022-03-14 14:44:13 +0100

    funktionierende Version

--- client/index.js
+++ client/index.js
@@ -2,10 +2,113 @@
 var csInterface = new CSInterface();
 
 /* 2) Make a reference to your HTML button and add a click handler. */
-var openButton = document.querySelector("#open-button");
-openButton.addEventListener("click", openDoc);
+var label1 = document.querySelector("#color1");
+label1.addEventListener("click", change1);
 
-/* 3) Write a helper function to pass instructions to the ExtendScript side. */
-function openDoc() {
-  csInterface.evalScript("openDocument()");
+function change1() {
+  csInterface.evalScript("changeLabel1()");
+}
+
+var label2 = document.querySelector("#color2");
+label2.addEventListener("click", change2);
+
+function change2() {
+  csInterface.evalScript("changeLabel2()");
+}
+var label3 = document.querySelector("#color3");
+label3.addEventListener("click", change3);
+
+function change3() {
+  csInterface.evalScript("changeLabel3()");
+}
+
+var label4 = document.querySelector("#color4");
+label4.addEventListener("click", change4);
+
+function change4() {
+  csInterface.evalScript("changeLabel4()");
+}
+
+var label5 = document.querySelector("#color5");
+label5.addEventListener("click", change5);
+
+function change5() {
+  csInterface.evalScript("changeLabel5()");
+}
+
+var label6 = document.querySelector("#color6");
+label6.addEventListener("click", change6);
+
+function change6() {
+  csInterface.evalScript("changeLabel6()");
+}
+
+var label7 = document.querySelector("#color7");
+label7.addEventListener("click", change7);
+
+function change7() {
+  csInterface.evalScript("changeLabel7()");
+}
+
+var label8 = document.querySelector("#color8");
+label8.addEventListener("click", change8);
+
+function change8() {
+  csInterface.evalScript("changeLabel8()");
+}
+
+var label9 = document.querySelector("#color9");
+label9.addEventListener("click", change9);
+
+function change9() {
+  csInterface.evalScript("changeLabel9()");
+}
+
+var label10 = document.querySelector("#color10");
+label10.addEventListener("click", change10);
+
+function change10() {
+  csInterface.evalScript("changeLabel10()");
+}
+
+var label11 = document.querySelector("#color11");
+label11.addEventListener("click", change11);
+
+function change11() {
+  csInterface.evalScript("changeLabel11()");
+}
+
+var label12 = document.querySelector("#color12");
+label12.addEventListener("click", change12);
+
+function change12() {
+  csInterface.evalScript("changeLabel12()");
+}
+
+var label13 = document.querySelector("#color13");
+label13.addEventListener("click", change13);
+
+function change13() {
+  csInterface.evalScript("changeLabel13()");
+}
+
+var label14 = document.querySelector("#color14");
+label14.addEventListener("click", change14);
+
+function change14() {
+  csInterface.evalScript("changeLabel14()");
+}
+
+var label15 = document.querySelector("#color15");
+label15.addEventListener("click", change15);
+
+function change15() {
+  csInterface.evalScript("changeLabel15()");
+}
+
+var label16 = document.querySelector("#color16");
+label16.addEventListener("click", change16);
+
+function change16() {
+  csInterface.evalScript("changeLabel16()");
 }
--- host/index.jsx
+++ host/index.jsx
@@ -1,3 +1,506 @@
-function openDocument(){
-    alert('Hello World');
+var labelColors = ["B53838", "E4D84C", "A9CBC7", "E5BCC9", "A9A9CA", "E7C19E", "B3C7B3", "677DE0", "4AA44C", "8E2C9A", "E8920D", "7F452A", "F46DD6", "3DA2A5", "A89677", "1E401E"];
+
+for(var i=0; i<labelColors.length; i++) {
+  var buttonColor = document.querySelector('#color' + (i+1));
+  var newColor = '#' + labelColors[i];
+  buttonColor.style.backgroundColor = newColor;
+}
+
+
+// Color-Changer
+function changeLabel1() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
   }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 1);
+}
+
+
+function changeLabel2() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 2);
+}
+
+function changeLabel3() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 3);
+}
+
+function changeLabel4() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 4);
+}
+
+function changeLabel5() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 5);
+}
+
+function changeLabel6() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 6);
+}
+
+function changeLabel7() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 7);
+}
+
+function changeLabel8() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 8);
+}
+
+function changeLabel9() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 9);
+}
+
+function changeLabel10() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 10);
+}
+
+function changeLabel11() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 11);
+}
+
+function changeLabel12() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 12);
+}
+
+function changeLabel13() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 13);
+}
+
+function changeLabel14() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 14);
+}
+
+function changeLabel15() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 15);
+}
+
+function changeLabel16() {
+  var proj = app.project;
+
+  if(proj.activeItem instanceof CompItem) {
+    // Hier die selected Layer auslesen und in Array zurückgeben
+    var items = getSelectedItems(proj.activeItem.layers);
+  } else {
+    // Hier die Items auslesen die selected sind und in Array zurückgeben
+    var items = getSelectedItems(proj.items);
+  }
+  
+  
+  function getSelectedItems(collectionObject) {
+      var selectedItems = [];
+      for(var i=1; i<=collectionObject.length; i++) {
+          if(collectionObject[i].selected) {
+          selectedItems.push(collectionObject[i]);
+          }
+      }
+      return selectedItems;
+  }
+  
+  function changeColor(itemsArray, colorNr) {
+    for(var i=0; i<itemsArray.length; i++){
+      itemsArray[i].label = colorNr;
+    }
+  }
+
+  changeColor(items, 16);
+}
