7adc8c247233e333580e17604877e71896668826
BrandAndreas <brand.unter3@gmail.com>
2022-03-15 15:25:42 +0100

    Code refactored

--- client/index.js
+++ client/index.js
@@ -1,114 +1,24 @@
 /* 1) Create an instance of CSInterface. */
 var csInterface = new CSInterface();
 
-/* 2) Make a reference to your HTML button and add a click handler. */
-var label1 = document.querySelector("#color1");
-label1.addEventListener("click", change1);
+//creating the addEventlisteners for all Buttons and give them the function with the color-Nr. as a parameter
+var buttons = document.querySelectorAll('button');
+buttons.forEach((button, index) => {
+  button.addEventListener('click', () => {
+    csInterface.evalScript("changeLabelColor("+(index+1)+")");
+  });
+})
 
-function change1() {
-  csInterface.evalScript("changeLabel1()");
+
+// labelColors are the original-colors of Adobe After Effects
+var labelColors = ["B53838", "E4D84C", "A9CBC7", "E5BCC9", "A9A9CA", "E7C19E", "B3C7B3", "677DE0", "4AA44C", "8E2C9A", "E8920D", "7F452A", "F46DD6", "3DA2A5", "A89677", "1E401E"];
+// Adding the # to the color-codes
+for(var j=0; j<labelColors.length; j++) {
+  labelColors[j] = '#' + labelColors[j];
 }
 
-var label2 = document.querySelector("#color2");
-label2.addEventListener("click", change2);
-
-function change2() {
-  csInterface.evalScript("changeLabel2()");
-}
-var label3 = document.querySelector("#color3");
-label3.addEventListener("click", change3);
-
-function change3() {
-  csInterface.evalScript("changeLabel3()");
-}
-
-var label4 = document.querySelector("#color4");
-label4.addEventListener("click", change4);
-
-function change4() {
-  csInterface.evalScript("changeLabel4()");
-}
-
-var label5 = document.querySelector("#color5");
-label5.addEventListener("click", change5);
-
-function change5() {
-  csInterface.evalScript("changeLabel5()");
-}
-
-var label6 = document.querySelector("#color6");
-label6.addEventListener("click", change6);
-
-function change6() {
-  csInterface.evalScript("changeLabel6()");
-}
-
-var label7 = document.querySelector("#color7");
-label7.addEventListener("click", change7);
-
-function change7() {
-  csInterface.evalScript("changeLabel7()");
-}
-
-var label8 = document.querySelector("#color8");
-label8.addEventListener("click", change8);
-
-function change8() {
-  csInterface.evalScript("changeLabel8()");
-}
-
-var label9 = document.querySelector("#color9");
-label9.addEventListener("click", change9);
-
-function change9() {
-  csInterface.evalScript("changeLabel9()");
-}
-
-var label10 = document.querySelector("#color10");
-label10.addEventListener("click", change10);
-
-function change10() {
-  csInterface.evalScript("changeLabel10()");
-}
-
-var label11 = document.querySelector("#color11");
-label11.addEventListener("click", change11);
-
-function change11() {
-  csInterface.evalScript("changeLabel11()");
-}
-
-var label12 = document.querySelector("#color12");
-label12.addEventListener("click", change12);
-
-function change12() {
-  csInterface.evalScript("changeLabel12()");
-}
-
-var label13 = document.querySelector("#color13");
-label13.addEventListener("click", change13);
-
-function change13() {
-  csInterface.evalScript("changeLabel13()");
-}
-
-var label14 = document.querySelector("#color14");
-label14.addEventListener("click", change14);
-
-function change14() {
-  csInterface.evalScript("changeLabel14()");
-}
-
-var label15 = document.querySelector("#color15");
-label15.addEventListener("click", change15);
-
-function change15() {
-  csInterface.evalScript("changeLabel15()");
-}
-
-var label16 = document.querySelector("#color16");
-label16.addEventListener("click", change16);
-
-function change16() {
-  csInterface.evalScript("changeLabel16()");
+// colorize the button
+for(var i=0; i<labelColors.length; i++) {
+  var buttonColor = document.querySelector('#color' + (i+1));
+  buttonColor.style.backgroundColor = labelColors[i];
 }
